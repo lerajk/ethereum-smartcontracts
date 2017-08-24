316738962279234cd530793cbec90851f637838e
lerajk <lerajk44@gmail.com>
2017-08-24 11:51:06 -0400

    second

--- custodial.sol
+++ custodial.sol
@@ -5,22 +5,34 @@ contract CustodialContract {
     address client;
     bool _switch = false;
     
+    event UpdateStatus(string _msg, uint _amount);
+    event UserStatus(string _msg, address user, uint _amount);
+    
     function CustodialContract()  {
         
         client = msg.sender;
         
     }
     
-     function depositFunds() payable {
-        
+    modifier ifClient(){
+        if(msg.sender != client){
+            throw;
+        } else {
+            _;
+        }
     }
     
-    function getFunds() constant returns(uint){
+     function depositFunds() payable {
+        UpdateStatus('Deposit Money = ', msg.value);
+        UserStatus('Address + Money = ', msg.sender, msg.value);
+    }
+    
+    function getFunds()  ifClient() constant returns(uint){
         
         return this.balance;
     }
     
-     function withdrawFunds(uint amount) {
+     function withdrawFunds(uint amount)  ifClient() {
          
          if(client.send(amount)){
              _switch = true;
