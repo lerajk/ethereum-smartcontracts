c8e122b9da3e2c16e3e58f855088e8beca361b2f
Lee Raj <lerajk44@gmail.com>
2017-11-04 23:38:06 -0400

    practice

---
+++ CrowdFunder.sol
@@ -0,0 +1,70 @@
+/**
+ * https://www.toshblocks.com/solidity/complete-example-crowd-funding-smart-contract/
+ */
+contract  CrowdFunder {
+
+	address creator;
+	address fundRecipient;
+	uint minimunToRaise;
+	uint totalRaised;
+	unit currentBalance;
+	uint raiseBy;
+	uint completeAt;
+	string url;
+
+
+
+	enum State { Fundraising, ExpiredRefund, Succesful, Closed}
+	State state = State.Fundraising;
+
+	modifier inState(State _state) { 
+		if (state != _state) throw; 
+		_; 
+	}
+
+	modifier isCreator() { 
+		if (msg.sender != creator) throw; 
+		_; 
+	}
+
+	modifier atEndOfLifeCycle() { 
+		if (!((state == State.ExpiredRefund || (state == State.Succesful) && (completeAt + 1))) throw; 
+		_; 
+	}
+	
+
+	struc Contribution {
+
+		uint amount;
+		address contributor;
+	}
+
+	Contribution[] contributions;
+	
+
+	function  CrowdFunder (uint timeInHoursForFundraising, string _campaignUrl, address _fundRecipient, uint _minimumToRaise) {
+
+		creator = msg.sender;
+		fundRecipient = _fundRecipient;
+		campaignUrl = _campaignUrl;
+		minimunToRaise = _minimumToRaise * 10 * 100000000000000000; 
+		raiseBy = now + (timeInHoursForFundraising * 1 hours);
+		currentBalance = 0;
+		
+	}	
+
+	function contribute() public inState(State.Fundraising) payable returns (uint256){
+
+		contributions.push(Contribution({
+
+				amount: msg.value;
+				contributor: msg.sender;
+			}));
+		totalRaised += msg.value;
+		currentBalance = totalRaised;
+
+		
+
+	}
+
+}
