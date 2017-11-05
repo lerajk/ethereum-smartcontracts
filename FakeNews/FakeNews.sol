397611a04690260ee6158cf5d0a4d3b475886000
Lee Raj <lerajk44@gmail.com>
2017-11-04 23:27:57 -0400

    practice

---
+++ FakeNews/FakeNews.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.15;
+
+Contract FakeNews {
+
+	struct Voter {
+
+		bool voted;
+		address delegate;
+		uint vote;
+	}
+
+
+	} // contract ends
--- insurance/insurance.sol
+++ insurance/insurance.sol
@@ -2,9 +2,56 @@ pragma solidity ^0.4.15;
 
 contract Lottery {
 
-	address[20] public players;
+	//address[20] public players;
+	address public participant; 
 	uint totalpot; 
+	uint bet;
+	
+	//mapping (address => uint256) balances;
 
+	
+	event sendplay(address indexed _from, uint _value);
+
+	function sendplaymoney (uint _bet) public returns (address){
+
+		participant = msg.sender;
+		totalpot += _bet;
+		sendplay(msg.sender, _bet);
+	    return participant;
+
+	}
+	
+	function potbalance() public returns (uint){
+	    
+	    return totalpot; 
+	}
+	
+	/*function guessNumber(uint _number) public returns (uint){
+	    
+	    participant = msg.sender;
+	    if(_number == 5)
+	    balances(participant, totalpot);
+	    //msg.value = totalpot;
+	    return totalpot;
+	} */
+	
+		function transfer(uint _number) public returns (bool){
+	    participant = msg.sender;
+	    if(_number == 5)
+	    participant.send(totalpot);
+	    return true;
+	 
+	    //participant = msg.sender;
+	    
+	    
+	}
+	
+	
+
+} //contract ends
+
+
+	/*
 	function () payable {
     sendplaymoney(msg.sender)
   }
@@ -13,6 +60,4 @@ contract Lottery {
     uint256 amount = msg.sender;
     totalpot += amount; 
   }
-
-
-} //contract ends
+	*/
